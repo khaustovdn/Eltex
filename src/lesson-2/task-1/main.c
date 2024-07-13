2140a95096521c2109edb6185d2c244f82ad38b1
Khaustov Danil <khaustovdn@vk.com>
2024-07-13 16:57:30 +0700

    eltex-2-1: output header

--- src/lesson-2/task-1/main.c
+++ src/lesson-2/task-1/main.c
@@ -2,8 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
+#define MAX_LEN 128
+
+char *get_chars (int n, char symblol);
+
+int
+main (int argc, char *argv[])
 {
-  printf ("Hello world");
+  printf ("%sContacts%s", get_chars (12, '-'), get_chars(12, '-'));
   return EXIT_SUCCESS;
+}
+
+char *
+get_chars (int n, char symbol)
+{
+  char *result = (char *)malloc (n * sizeof (char));
+  for (int i = 0; i < n; i++) {
+    *(result + i) = symbol;
+  }
+  return result;
 }
