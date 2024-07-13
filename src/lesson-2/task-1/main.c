a2b52496dac6ca39c7c4ffbd90c98a645c44b7bb
Khaustov Danil <khaustovdn@vk.com>
2024-07-13 23:52:42 +0700

    eltex-2-1: main menu

--- src/lesson-2/task-1/main.c
+++ src/lesson-2/task-1/main.c
@@ -1,24 +1,68 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define MAX_LEN 128
+char* create_wrapped_title(const char* title, int width, char symbol);
+char get_action_choice();
 
-char *get_chars (int n, char symblol);
+int main(int argc, char* argv[]) {
+  char* wrapped_title = create_wrapped_title("Contacts", 30, '-');
+  if (wrapped_title == NULL) {
+    return EXIT_FAILURE;
+  }
+  puts(wrapped_title);
+  free(wrapped_title);
 
-int
-main (int argc, char *argv[])
-{
-  printf ("%sContacts%s", get_chars (12, '-'), get_chars(12, '-'));
+  for (;;) {
+    char action_choice = get_action_choice();
+    switch (action_choice) {
+      case 'a':
+        puts("Append contact action chosen");
+        break;
+      case 'e':
+        puts("Edit contact info action chosen");
+        break;
+      case 'r':
+        puts("Remove contact action chosen");
+        break;
+      case 'q':
+        return EXIT_SUCCESS;
+      default:
+        puts("Invalid choice of action");
+    }
+  }
+
   return EXIT_SUCCESS;
 }
 
-char *
-get_chars (int n, char symbol)
-{
-  char *result = (char *)malloc (n * sizeof (char));
-  for (int i = 0; i < n; i++) {
-    *(result + i) = symbol;
+char* create_wrapped_title(const char* title, int width, char symbol) {
+  if (width < strlen(title)) {
+    fputs("Error: Width is too small for the title", stdout);
+    return NULL;
   }
+  size_t wrap_width = (width - strlen(title)) >> 1;
+
+  char* result = (char*)malloc((width + 1) * sizeof(char));
+  if (result == NULL) {
+    fputs("Error: Failed to allocate memory", stdout);
+    return NULL;
+  }
+
+  memset(result, symbol, width);
+  result[width] = '\0';
+
+  for (int i = 0; i < strlen(title); i++)
+    result[i + wrap_width] = title[i];
+
   return result;
+}
+
+char get_action_choice() {
+  char action_choice;
+  fputs("Choose an action:\n\ta. Append contact\n\te. Edit contact info\n\tr. Remove contact\n\tq. Quit\nInput: ", stdout);
+  action_choice = fgetc(stdin);
+  while (fgetc(stdin) != '\n')
+    ;
+  return action_choice;
 }
