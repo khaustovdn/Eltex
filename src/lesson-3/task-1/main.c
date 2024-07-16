89925eeb34a72e3c662b8ad8ea6a488c8a1649bf
Khaustov Danil <khaustovdn@vk.com>
2024-07-16 19:10:38 +0700

    eltex-3-1: hello world

---
+++ src/lesson-3/task-1/main.c
@@ -0,0 +1,12 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "main.h"
+
+int
+main(int argc, char *argv[])
+{
+  puts("Hello world");
+  return EXIT_SUCCESS;
+}
---
+++ src/lesson-3/task-1/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+int
+main(int args, char *argv[]);
+
+#endif // MAIN_H_
