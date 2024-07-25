b136daef69679378c42c5ed52b08d9f8e80fa522
khaustovdn <khaustovdn@vk.com>
2024-07-26 00:20:55 +0700

    eltex-0-2: main menu

--- src/module-3/lesson-0/task-2/main.c
+++ src/module-3/lesson-0/task-2/main.c
@@ -1,9 +1,44 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char *argv[])
+#include "main.h"
+
+int
+main(int argc, char* argv[])
 {
-  puts("Hello, World!");
+  CommandEntry commands[] = { { "summator", "summator" },
+                              { "string-joiner", "string_joiner" },
+                              { "max-number-finder", "max_number_finder" },
+                              { "max-line-finder", "max_line_finder" },
+                              { NULL, NULL } };
+
+  char* action_choice;
+
+  while (strncmp((action_choice = command_interpreter_menu()), "q", MAX_LEN) !=
+         0) {
+    int i = 0;
+    for (; commands[i].name != NULL; i++) {
+      char* name = strtok(action_choice, " ");
+      if (strncmp(name, commands[i].name, MAX_LEN) == 0) {
+        puts("Good");
+        break;
+      }
+    }
+    if (commands[i].name == NULL)
+      puts("Invalid choice of action");
+  }
   return EXIT_SUCCESS;
+}
+
+char*
+command_interpreter_menu()
+{
+  output_wrapped_title("Command Interpreter Menu", 50, '-');
+
+  fputs("Choose an command:\n\t- summator\n\t- string-joiner\n\t"
+        "- max-number-finder\n\t- max-line-finder\n\tq. Quit\nInput: ",
+        stdout);
+  return input_string();
 }
---
+++ src/module-3/lesson-0/task-2/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include "../../../utils/utils.h"
+
+int
+main(int argc, char* argv[]);
+
+char*
+command_interpreter_menu();
+
+#endif // MAIN_H_
