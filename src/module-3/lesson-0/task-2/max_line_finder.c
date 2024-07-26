c3753121c1fdde73283c7674712fb9cc61858afd
khaustovdn <khaustovdn@vk.com>
2024-07-26 13:32:09 +0700

    eltex-0-2: add max_line_finder

---
+++ src/module-3/lesson-0/task-2/max_line_finder.c
@@ -0,0 +1,36 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../../../utils/utils.h"
+
+int
+main(int argc, char* argv[])
+{
+  if (argc < 3) {
+    puts("Error: Incorrect number of arguments");
+    exit(EXIT_FAILURE);
+  }
+
+  InputResult result;
+  {
+    result.value = (char*)malloc(sizeof(char));
+    if (result.value == NULL) {
+      fprintf(stderr, "Error: Memory allocation failed.\n");
+      exit(EXIT_FAILURE);
+    }
+    result.success = false;
+  }
+
+  for (int i = 1; i < argc; i++) {
+    strncpy(result.value, (result.success == false || strlen(result.value) < strlen(argv[i])) ?
+            argv[i] :
+            result.value, MAX_LEN);
+    result.success = true;
+  }
+
+  printf("Result: %s. Count: %lu\n", (char*)result.value, strlen(result.value));
+
+  return EXIT_SUCCESS;
+}
---
+++ src/module-3/lesson-0/task-2/max_line_finder.h
@@ -0,0 +1,7 @@
+#ifndef MAX_LINE_FINDER_H_
+#define MAX_LINE_FINDER_H_
+
+int
+main(int argc, char* argv[]);
+
+#endif // MAX_LINE_FINDER_H_
