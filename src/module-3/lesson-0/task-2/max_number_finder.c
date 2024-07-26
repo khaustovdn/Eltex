3ffaa581e67813266b362e3ab3d4458b521fbf02
khaustovdn <khaustovdn@vk.com>
2024-07-26 13:31:51 +0700

    eltex-0-2: add max_number_finder

---
+++ src/module-3/lesson-0/task-2/max_number_finder.c
@@ -0,0 +1,40 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../../../utils/utils.h"
+
+int
+main(int argc, char* argv[])
+{
+  if (argc < 3) {
+    puts("Error: Incorrect number of arguments");
+    exit(EXIT_FAILURE);
+  }
+
+  InputResult result;
+  {
+    result.value = (int*)malloc(sizeof(int));
+    if (result.value == NULL) {
+      fprintf(stderr, "Error: Memory allocation failed.\n");
+      exit(EXIT_FAILURE);
+    }
+    result.success = false;
+  }
+
+  for (int i = 1; i < argc; i++) {
+    if (is_integer(argv[i]) == false) {
+      printf("The \"%s\" argument numbered %d is not a number\n", argv[i], i);
+      continue;
+    }
+    *(int*)result.value =
+      (result.success == false || *(int*)result.value < atoi(argv[i]))
+        ? atoi(argv[i])
+        : *(int*)result.value;
+    result.success = true;
+  }
+
+  printf("Result: %d\n", *(int*)result.value);
+
+  return EXIT_SUCCESS;
+}
---
+++ src/module-3/lesson-0/task-2/max_number_finder.h
@@ -0,0 +1,7 @@
+#ifndef MAX_NUMBER_FINDER_H_
+#define MAX_NUMBER_FINDER_H_
+
+int
+main(int argc, char* argv[]);
+
+#endif // MAX_NUMBER_FINDER_H_
