457cc31c1e80e4310346ea4b6e5554c5349b9202
khaustovdn <khaustovdn@vk.com>
2024-07-30 02:01:59 +0700

    eltex-0-7: add the ability to send and receive messages

--- src/module-3/lesson-2/task-7/server.c
+++ src/module-3/lesson-2/task-7/server.c
@@ -6,7 +6,8 @@
 
 #include "../../../utils/utils.h"
 
-#define QUEUE_NAME "/prof"
+#define SERVER_QUEUE_NAME "/server"
+#define SERVER_QUIT_QUEUE_NAME "/server-quit"
 #define MAX_MESSAGES 10
 
 #define SERVER_QUIT_MESSAGE_PRIORITY 1
@@ -14,38 +15,67 @@
 #define SERVER_MESSAGE_PRIORITY 3
 #define CLIENT_MESSAGE_PRIORITY 4
 
+void
+receive_messages(mqd_t qd[])
+{
+  output_wrapped_title("Output", 30, '-');
+
+  unsigned int priority;
+  char message[MAX_LEN];
+
+  while (mq_receive(qd[0], message, MAX_LEN, &priority) != -1) {
+    printf("Server process received: %s\n", message);
+  }
+}
+
+void
+send_message(mqd_t qd[],
+             const char* value,
+             unsigned int priority)
+{
+  if (mq_send(qd[0], value, MAX_LEN, priority) == -1) {
+    perror("mq_send");
+    mq_close(qd[0]);
+    mq_close(qd[1]);
+    mq_unlink(SERVER_QUEUE_NAME);
+    exit(EXIT_FAILURE);
+  }
+}
+
 char*
 server_menu()
 {
   output_wrapped_title("Server Menu", 50, '-');
 
-  fputs("Enter the number to send\n\tq. Quit\nInput: ",
-        stdout);
+  fputs("Enter the number to send\n\tq. Quit\nInput: ", stdout);
   return input_string();
 }
 
 int
 main(int argc, char* argv[])
 {
-  mqd_t qd_server, qd_client;
-  long token_number = 1;
+  mqd_t qd_server[2];
 
-  struct mq_attr attr;
+  struct mq_attr queue_attr;
 
-  attr.mq_flags = 0;
-  attr.mq_maxmsg = MAX_MESSAGES;
-  attr.mq_msgsize = MAX_LEN;
-  attr.mq_curmsgs = 0;
+  queue_attr.mq_flags = 0;
+  queue_attr.mq_maxmsg = MAX_MESSAGES;
+  queue_attr.mq_msgsize = MAX_LEN;
+  queue_attr.mq_curmsgs = 0;
 
-  if ((qd_server = mq_open(QUEUE_NAME,
-                           O_WRONLY | O_CREAT,
-                           SERVER_MESSAGE_PRIORITY,
-                           &attr)) == -1) {
-    perror("mq_open");
+  mq_unlink(SERVER_QUEUE_NAME);
+  if ((qd_server[0] =
+         mq_open(SERVER_QUEUE_NAME,
+                 O_RDWR | O_CREAT | O_NONBLOCK | 0666,
+                 SERVER_MESSAGE_PRIORITY,
+                 &queue_attr)) == -1) {
+    perror("mq_open (server)");
     exit(1);
   }
 
   for (;;) {
+    receive_messages(qd_server);
+
     char* action_choice = server_menu();
     if (strncmp(action_choice, "q", MAX_LEN) == 0) {
       puts("\nThe server has successfully shut down");
@@ -59,9 +89,14 @@ main(int argc, char* argv[])
       continue;
     }
 
+    send_message(
+      qd_server, action_choice, SERVER_MESSAGE_PRIORITY);
+
     free(action_choice);
   }
 
-  mq_unlink(QUEUE_NAME);
+  mq_close(qd_server[0]);
+  mq_close(qd_server[1]);
+  mq_unlink(SERVER_QUEUE_NAME);
   return EXIT_SUCCESS;
 }
