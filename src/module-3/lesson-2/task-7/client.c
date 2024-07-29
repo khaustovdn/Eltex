c4ba19d7e35e3f6d4992cbad4a833bec062b98b4
khaustovdn <khaustovdn@vk.com>
2024-07-30 05:56:59 +0700

    eltex-0-7: added channels with higher priority for output

--- src/module-3/lesson-2/task-7/client.c
+++ src/module-3/lesson-2/task-7/client.c
@@ -4,12 +4,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 
 #include "../../../utils/utils.h"
 
 #define SERVER_QUEUE_NAME "/server"
 #define CLIENT_QUEUE_NAME "/client"
+#define SERVER_QUIT_QUEUE_NAME "/server-quit"
+#define CLIENT_QUIT_QUEUE_NAME "/client-quit"
 #define MAX_MESSAGES 10
 
 #define SERVER_QUIT_MESSAGE_PRIORITY 1
@@ -19,21 +20,28 @@
 
 #define QUEUE_PERMISSIONS 0660
 
-void
-receive_messages(mqd_t qd)
+unsigned int
+receive_messages(mqd_t qd[])
 {
   output_wrapped_title("Output", 30, '-');
 
   unsigned int priority;
   char message[MAX_LEN];
 
-  while (mq_receive(qd, message, MAX_LEN, &priority) != -1) {
+  while (mq_receive(qd[1], message, MAX_LEN, &priority) != -1) {
+    puts("\nThe client has successfully shut down");
+    return priority;
+  }
+
+  while (mq_receive(qd[0], message, MAX_LEN, &priority) != -1) {
     printf("Client process received: %s\n", message);
   }
 
   if (errno != EAGAIN) {
     perror("mq_receive");
   }
+
+  return priority;
 }
 
 char*
@@ -48,33 +56,46 @@ client_menu()
 int
 main(int argc, char* argv[])
 {
-  mqd_t qd_client;
+  mqd_t qd_client[2];
 
   struct mq_attr queue_attr = { .mq_flags = 0,
                                 .mq_maxmsg = MAX_MESSAGES,
                                 .mq_msgsize = MAX_LEN,
                                 .mq_curmsgs = 0 };
 
-  qd_client = mq_open(CLIENT_QUEUE_NAME,
-                      O_RDONLY | O_CREAT | O_NONBLOCK,
-                      QUEUE_PERMISSIONS,
-                      &queue_attr);
-  if (qd_client == (mqd_t)-1) {
+  qd_client[0] = mq_open(CLIENT_QUEUE_NAME,
+                         O_RDONLY | O_CREAT | O_NONBLOCK,
+                         QUEUE_PERMISSIONS,
+                         &queue_attr);
+  qd_client[1] = mq_open(CLIENT_QUIT_QUEUE_NAME,
+                         O_RDONLY | O_CREAT | O_NONBLOCK,
+                         QUEUE_PERMISSIONS,
+                         &queue_attr);
+  if (qd_client[0] == (mqd_t)-1) {
     perror("mq_open (client)");
+    exit(EXIT_FAILURE);
+  }
+  if (qd_client[1] == (mqd_t)-1) {
+    perror("mq_open (client quit)");
     exit(EXIT_FAILURE);
   }
 
   for (;;) {
-    receive_messages(qd_client);
+    if (receive_messages(qd_client) ==
+        CLIENT_QUIT_MESSAGE_PRIORITY)
+      break;
 
     char* action_choice = client_menu();
-    if (!action_choice) {
-      fprintf(stderr, "Failed to read input\n");
-      continue;
-    }
 
-    mqd_t qd_server = mq_open(SERVER_QUEUE_NAME, O_WRONLY);
-    if (qd_server == (mqd_t)-1) {
+    mqd_t qd_server[2];
+    qd_server[0] = mq_open(SERVER_QUEUE_NAME, O_WRONLY);
+    if (qd_server[0] == (mqd_t)-1) {
+      perror("mq_open (server)");
+      free(action_choice);
+      break;
+    }
+    qd_server[1] = mq_open(SERVER_QUIT_QUEUE_NAME, O_WRONLY);
+    if (qd_server[1] == (mqd_t)-1) {
       perror("mq_open (server)");
       free(action_choice);
       break;
@@ -82,13 +103,13 @@ main(int argc, char* argv[])
 
     if (strncmp(action_choice, "q", MAX_LEN) == 0) {
       puts("\nThe client has successfully shut down");
-      if (mq_send(qd_server,
+      if (mq_send(qd_server[1],
                   action_choice,
                   MAX_LEN,
-                  SERVER_QUIT_MESSAGE_PRIORITY) == -1) {
-        perror("mq_send (to server quit)");
-      }
-      mq_close(qd_server);
+                  SERVER_QUIT_MESSAGE_PRIORITY) == -1)
+        perror("mq_send (to server)");
+      mq_close(qd_server[0]);
+      mq_close(qd_server[1]);
       free(action_choice);
       break;
     }
@@ -99,18 +120,20 @@ main(int argc, char* argv[])
       continue;
     }
 
-    if (mq_send(qd_server,
+    if (mq_send(qd_server[0],
                 action_choice,
                 MAX_LEN,
                 SERVER_MESSAGE_PRIORITY) == -1) {
       perror("mq_send (to server)");
     }
 
-    mq_close(qd_server);
+    mq_close(qd_server[0]);
+    mq_close(qd_server[1]);
     free(action_choice);
   }
 
-  mq_close(qd_client);
+  mq_close(qd_client[0]);
+  mq_close(qd_client[1]);
 
   return EXIT_SUCCESS;
 }
--- src/module-3/lesson-2/task-7/server.c
+++ src/module-3/lesson-2/task-7/server.c
@@ -9,6 +9,8 @@
 
 #define SERVER_QUEUE_NAME "/server"
 #define CLIENT_QUEUE_NAME "/client"
+#define SERVER_QUIT_QUEUE_NAME "/server-quit"
+#define CLIENT_QUIT_QUEUE_NAME "/client-quit"
 #define MAX_MESSAGES 10
 
 #define SERVER_QUIT_MESSAGE_PRIORITY 1
@@ -18,21 +20,28 @@
 
 #define QUEUE_PERMISSIONS 0660
 
-void
-receive_messages(mqd_t qd)
+unsigned int
+receive_messages(mqd_t qd[])
 {
   output_wrapped_title("Output", 30, '-');
 
   unsigned int priority;
   char message[MAX_LEN];
 
-  while (mq_receive(qd, message, MAX_LEN, &priority) != -1) {
+  while (mq_receive(qd[1], message, MAX_LEN, &priority) != -1) {
+    puts("\nThe server has successfully shut down");
+    return priority;
+  }
+
+  while (mq_receive(qd[0], message, MAX_LEN, &priority) != -1) {
     printf("Server process received: %s\n", message);
   }
 
   if (errno != EAGAIN) {
     perror("mq_receive");
   }
+
+  return priority;
 }
 
 char*
@@ -47,33 +56,46 @@ server_menu()
 int
 main(int argc, char* argv[])
 {
-  mqd_t qd_server;
+  mqd_t qd_server[2];
 
   struct mq_attr queue_attr = { .mq_flags = 0,
                                 .mq_maxmsg = MAX_MESSAGES,
                                 .mq_msgsize = MAX_LEN,
                                 .mq_curmsgs = 0 };
 
-  qd_server = mq_open(SERVER_QUEUE_NAME,
-                      O_RDONLY | O_CREAT | O_NONBLOCK,
-                      QUEUE_PERMISSIONS,
-                      &queue_attr);
-  if (qd_server == (mqd_t)-1) {
+  qd_server[0] = mq_open(SERVER_QUEUE_NAME,
+                         O_RDONLY | O_CREAT | O_NONBLOCK,
+                         QUEUE_PERMISSIONS,
+                         &queue_attr);
+  qd_server[1] = mq_open(SERVER_QUIT_QUEUE_NAME,
+                         O_RDONLY | O_CREAT | O_NONBLOCK,
+                         QUEUE_PERMISSIONS,
+                         &queue_attr);
+  if (qd_server[0] == (mqd_t)-1) {
     perror("mq_open (server)");
+    exit(EXIT_FAILURE);
+  }
+  if (qd_server[1] == (mqd_t)-1) {
+    perror("mq_open (server quit)");
     exit(EXIT_FAILURE);
   }
 
   for (;;) {
-    receive_messages(qd_server);
+    if (receive_messages(qd_server) ==
+        SERVER_QUIT_MESSAGE_PRIORITY)
+      break;
 
     char* action_choice = server_menu();
-    if (!action_choice) {
-      fprintf(stderr, "Failed to read input\n");
-      continue;
-    }
 
-    mqd_t qd_client = mq_open(CLIENT_QUEUE_NAME, O_WRONLY);
-    if (qd_client == (mqd_t)-1) {
+    mqd_t qd_client[2];
+    qd_client[0] = mq_open(CLIENT_QUEUE_NAME, O_WRONLY);
+    if (qd_client[0] == (mqd_t)-1) {
+      perror("mq_open (client)");
+      free(action_choice);
+      break;
+    }
+    qd_client[1] = mq_open(CLIENT_QUIT_QUEUE_NAME, O_WRONLY);
+    if (qd_client[1] == (mqd_t)-1) {
       perror("mq_open (client)");
       free(action_choice);
       break;
@@ -81,12 +103,13 @@ main(int argc, char* argv[])
 
     if (strncmp(action_choice, "q", MAX_LEN) == 0) {
       puts("\nThe server has successfully shut down");
-      if (mq_send(qd_client,
+      if (mq_send(qd_client[1],
                   action_choice,
                   MAX_LEN,
                   CLIENT_QUIT_MESSAGE_PRIORITY) == -1)
         perror("mq_send (to client)");
-      mq_close(qd_client);
+      mq_close(qd_client[0]);
+      mq_close(qd_client[1]);
       free(action_choice);
       break;
     }
@@ -97,18 +120,20 @@ main(int argc, char* argv[])
       continue;
     }
 
-    if (mq_send(qd_client,
+    if (mq_send(qd_client[0],
                 action_choice,
                 MAX_LEN,
                 CLIENT_MESSAGE_PRIORITY) == -1) {
       perror("mq_send (to client)");
     }
 
-    mq_close(qd_client);
+    mq_close(qd_client[0]);
+    mq_close(qd_client[1]);
     free(action_choice);
   }
 
-  mq_close(qd_server);
+  mq_close(qd_server[0]);
+  mq_close(qd_server[1]);
   mq_unlink(SERVER_QUEUE_NAME);
   mq_unlink(CLIENT_QUEUE_NAME);
 
